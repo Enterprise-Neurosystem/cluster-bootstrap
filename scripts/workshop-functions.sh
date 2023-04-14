34a3778953533b5d342f911ee75494318f0a9ac1
Cory Latschkowski <koree@redhat.com>
2023-04-14 17:41:35 -0500

    update: workshop scripts

--- scripts/workshop-functions.sh
+++ scripts/workshop-functions.sh
@@ -3,6 +3,7 @@
 USER=user
 PASS=ThisIsFine
 GROUP=workshop-users
+HTPASSWD=htpasswd-workshop-secret
 TMP_DIR=scratch
 
 check_init(){
@@ -11,9 +12,9 @@ check_init(){
 
   # create generated folder
   [ ! -d ${TMP_DIR} ] && mkdir -p ${TMP_DIR}
-  }
+}
 
-  create_user_htpasswd(){
+workshop_create_user_htpasswd(){
   FILE=${TMP_DIR}/htpasswd
   touch ${FILE}
 
@@ -24,12 +25,12 @@ check_init(){
     htpasswd -bB ${FILE} "${USER}${i}" "${PASS}${i}"
   done
 
-  oc -n openshift-config create secret generic ${GROUP} --from-file=${FILE}
-  oc -n openshift-config set data secret/${GROUP} --from-file=${FILE}
+  oc -n openshift-config create secret generic ${HTPASSWD} --from-file=${FILE}
+  oc -n openshift-config set data secret/${HTPASSWD} --from-file=${FILE}
 
-  }
+}
 
-  create_user_ns(){
+workshop_create_user_ns(){
   OBJ_DIR=${TMP_DIR}/users
   [ ! -d ${OBJ_DIR} ] && mkdir -p ${OBJ_DIR}
 
@@ -38,6 +39,7 @@ check_init(){
     # create ns
     oc -o yaml --dry-run=client \
       create ns "${USER}${i}" > "${OBJ_DIR}/${USER}${i}-ns.yml"
+    oc apply -f ${OBJ_DIR}
 
     # create role binding - admin for user
     oc -o yaml --dry-run=client \
@@ -55,22 +57,41 @@ check_init(){
   done
 
   # apply objects created in scratch dir
-  oc apply -f ${OBJ_DIR}
-  }
+    oc apply -f ${OBJ_DIR}
+ }
 
-  clean_user_ns(){
+workshop_clean_user_ns(){
   for i in {0..20}
   do
     oc delete project "${USER}${i}"
   done
-  }
-
-  clean_misc(){
-  oc -n openshift-config \
-    delete secret ${GROUP}
-
-  oc delete group ${GROUP}
 }
 
+workshop_clean_user_notebooks(){
+  oc -n rhods-notebooks \
+    delete po -l app=jupyterhub
+}
+
+workshop_setup(){
+  check_init
+  workshop_create_user_htpasswd
+  workshop_create_user_ns
+}
+
+workshop_clean(){
+  echo "Workshop: Clean User Namespaces"
+  check_init
+  workshop_clean_user_ns
+  workshop_clean_user_notebooks
+}
+
+workshop_reset(){
+  echo "Workshop: Reset"
+  check_init
+  workshop_clean
+  workshop_setup
+}
 
 echo "Workshop: Functions Loaded"
+echo ""
+echo "usage: workshop_[setup,reset,clean]"
