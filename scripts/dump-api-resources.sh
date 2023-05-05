14f0a0213a78a0b3aa70d92de82cbf9039a66e9c
Cory Latschkowski <koree@redhat.com>
2023-05-05 17:23:39 -0500

    fix: api script

--- scripts/dump-api-resources.sh
+++ scripts/dump-api-resources.sh
@@ -4,20 +4,20 @@ SERVER="localhost:8080"
 oc proxy -p 8080 &
 sleep 3
 
-APIS=$(curl -s $SERVER/apis | jq -r '[.groups | .[].name] | join(" ")')
+APIS=$(curl -s "${SERVER}/apis" | jq -r '[.groups | .[].name] | join(" ")')
 
 # do core resources first, which are at a separate api location
 k8s_api_dump_core(){
   api="core"
-  curl -s $SERVER/api/v1 | \
+  curl -s "${SERVER}/api/v1" | \
     jq -r --arg api "$api" \
     '.resources | .[] | "\($api) \(.name): [ \(.verbs | join(",")) ]"'
 }
 # now do non-core resources
 k8s_api_dump_noncore(){
-  for api in $APIS; do
-    version=$(curl -s $SERVER/apis/$api | jq -r '.preferredVersion.version')
-    curl -s $SERVER/apis/$api/$version | \
+  for api in ${APIS}; do
+    version=$(curl -s "$SERVER/apis/${api}" | jq -r '.preferredVersion.version')
+    curl -s "${SERVER}/apis/${api}/${version}" | \
       jq -r --arg api "$api" \
       '.resources | .[]? | "\($api) \(.name): [ \(.verbs | join(",")) ]"'
   done
